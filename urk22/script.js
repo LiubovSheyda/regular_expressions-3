9c5b502501e5bd1a6477dfb94a049729512cf313
LiubovSheyda <liubovsheyda@gmail.com>
2020-03-23 13:06:44 +0700

    Update script.js

--- urk22/script.js
+++ urk22/script.js
@@ -1,4 +1,8 @@
 // через параграфы не вышло, поэтому сразу документ врайт
+
+/*
+выражение должно быть универсальным
+*/
 let strok = 'sss site.ru zzz site.com kkk';
 let yoy = strok.replace(/\w+\.com|\w+\.ru/g, '<a href="http://$&">$&</a>');
 document.write('ссылочки: ');
